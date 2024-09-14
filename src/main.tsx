9f1480ce9ce6f32dd99b51173e66a700e38d85ee
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-09-15 00:04:33 +0530

    added convex

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ConvexProvider>
   </StrictMode>,
 );
