9d797c4c455d09df7a192529d4453d8bfffd68fa
pr4j3sh <prajesh.eleven118@gmail.com>
2025-03-18 18:02:16 +0530

    re init repo

--- convex/auth.ts
+++
@@ -1,8 +0,0 @@
-import { convexAuth } from "@convex-dev/auth/server";
-import GitHub from "@auth/core/providers/github";
-import Google from "@auth/core/providers/google";
-import { Resend } from "./resend";
-
-export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [Google, GitHub, Resend],
-});
