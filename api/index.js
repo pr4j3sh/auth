f528cb5b4fb19c93cb8eeb84eae2e0c8d3dfc75d
pr4j3sh <prajesh.eleven118@gmail.com>
2025-03-21 23:48:42 +0530

    fix: rm logs

--- api/index.js
+++ api/index.js
@@ -1,12 +1,8 @@
 const {
   errorHandler,
   notFoundHandler,
-  logHandler,
   asyncHandler,
   corsHandler,
-  initLogger,
-  transports,
-  streamHandler,
   rateLimitHandler,
   tokenHandler,
   passwordHandler,
@@ -22,11 +18,9 @@ const hostname = process.env.HOSTNAME;
 const origins = process.env.ORIGINS;
 
 const server = express();
-const logger = initLogger("info", transports);
 
 server.use(express.json());
 server.use(corsHandler(origins));
-server.use(logHandler("combined", { stream: streamHandler(logger) }));
 server.use(rateLimitHandler({ windowMs: 10 * 60 * 1000, limit: 100 }));
 
 server.get(
@@ -135,7 +129,7 @@ server.use(errorHandler);
 
 server.listen(port, hostname, async () => {
   await mongoHandler(process.env.MONGO_URI);
-  logger.info(`server running @ http://${hostname}:${port}`);
+  console.log(`server running @ http://${hostname}:${port}`);
 });
 
 module.exports = server;
